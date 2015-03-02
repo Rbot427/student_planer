e666d47d1ecd486db933f7b46532c700b88d6821
Ryan <Ryan@Home-iMac-2.local>
2015-03-02 06:48:06 -0500

    Initial Commit

---
+++ Student Planer/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  Student Planer
+//
+//  Created by Ryan on 3/2/15.
+//  Copyright (c) 2015 Ryan Stentz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
---
+++ Student Planer/SecondViewController.m
@@ -0,0 +1,27 @@
+//
+//  SecondViewController.m
+//  Student Planer
+//
+//  Created by Ryan on 3/2/15.
+//  Copyright (c) 2015 Ryan Stentz. All rights reserved.
+//
+
+#import "SecondViewController.h"
+
+@interface SecondViewController ()
+
+@end
+
+@implementation SecondViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
